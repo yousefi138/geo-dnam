988bed020e0bcb73c620840663b3381f56ce4150
yousefi138 <paul.yousefi@bristol.ac.uk>
2025-07-28 08:25:33 +0100

    starting the report

---
+++ report.r
@@ -0,0 +1,7 @@
+## ----globals -------------------------------------------------------------
+packages <- c("kable", "kableExtra",
+            "eval.save")
+lapply(packages, require, character.only=T)
+
+## ----load.data -------------------------------------------------------------
+
